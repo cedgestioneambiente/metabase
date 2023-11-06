a26daae1b85707b28ed25357f5a0e4ccaf7edce7
Braden Shepherdson <braden@metabase.com>
2023-11-06 16:02:12 -0500

    Drop a broken test that needs v48 test infra changes

--- enterprise/backend/test/metabase_enterprise/serialization/v2/entity_ids_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/v2/entity_ids_test.clj
@@ -58,18 +58,4 @@
           (testing "Should return truthy on success"
             (is (= true
                    (v2.entity-ids/drop-entity-ids!))))
-          (is (nil? (entity-id)))))))
-  (testing "empty table"
-    (testing "has no entity ids"
-      (t2.with-temp/with-temp [Collection _ {:name       "No Entity ID Collection"
-                                             :slug       "no_entity_id_collection"}]
-        (is (= 0 (t2/count Dashboard)))
-        (let [eids (t2/select-fn-set :entity-id Dashboard)]
-          (is (or (empty? eids)
-                  (= #{nil} eids))))
-        (testing "but doesn't crash drop-entity-ids"
-          (is (= true
-                 (v2.entity-ids/drop-entity-ids!)))
-          (let [eids (t2/select-fn-set :entity-id Dashboard)]
-            (is (or (empty? eids)
-                    (= #{nil} eids)))))))))
+          (is (nil? (entity-id))))))))
